c3e80e91c5758db9fde2ee2df30dab40cbf8d369
Zheng Xiang <wong.zhengxiang@gmail.com>
2023-07-20 14:45:45 +0800

    Created boilerplate for setting types and getters.

---
+++ ex00/ScalarConverter.cpp
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zwong <zwong@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/20 14:37:02 by zwong             #+#    #+#             */
+/*   Updated: 2023/07/20 14:42:13 by zwong            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter() {
+    std::cout << "ScalarConverter: Default constructor called" << std::endl;
+    _type = NONE;
+}
+
+ScalarConverter::ScalarConverter(std::string str) {
+    std::cout << "ScalarConverter: Str constructor called" << std::endl;
+}
+
+ScalarConverter::ScalarConverter(const ScalarConverter &copy) {
+    std::cout << "ScalarConverter: Copy constructor called" << std::endl;
+    _input = copy.get_input();
+    _type = copy.get_type();
+}
+
+ScalarConverter::~ScalarConverter() { std::cout << "ScalarConverter: Default destructor called" << std::endl; }
+
+ScalarConverter &ScalarConverter::operator=(const ScalarConverter &other) {
+    std::cout << "ScalarConverter: copy assignment operator called" << std::endl;
+    _input = other.get_input();
+    _type = other.get_type();
+    return (*this);
+}
+
+// Getters
+std::string ScalarConverter::get_input() const {
+    return (_input);
+}
+
+t_type ScalarConverter::get_type() const {
+    return (_type);
+}
+
+// Setter
+
+// Functions NEXT: Do convert functions
+void ScalarConverter::convert(const std::string &input) {
+    return;
+}
+
+// PRIVATE functions
+void ScalarConverter::_main_set_type() {
+    _type = INVALID;
+    // Check for char
+    if (_input.length() == 1 && !std::isdigit(_input[0])) {
+        _type = CHAR;
+        return ;
+    }
+    // Check first for "nan" or "nanf"
+    else if (_input == "nan" || _input == "nanf") {
+        _type = MY_NAN;
+        return ;
+    }
+    // Check for all the "inf"
+    else if (_input == "+inf" || _input == "-inf" || _input == "+inff" || _input == "-inff") {
+        _type = INF;
+        return ;
+    }
+    // Else, just assume it is int, float, or double type
+    else
+        _set_type_int_float_double();
+}
+
+void ScalarConverter::_set_type_int_float_double() {
+    int i = 0;
+}
---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zwong <zwong@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/20 14:36:50 by zwong             #+#    #+#             */
+/*   Updated: 2023/07/20 14:40:32 by zwong            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <string>
+#include <iostream>
+#include <iomanip>
+
+#define DEFAULT "\033[39m"
+#define BLACK "\033[30m"
+#define GREY "\033[90m"
+#define RED "\033[31m"
+#define CYAN "\033[36m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[94m"
+#define MAGENTA "\033[35m"
+
+// char e.g. c, a
+// int e.g. 0, -42, 42
+// float e.g. 0.0f, -4.2f, 4.2f, -inff, +inff
+// double e.g. 0.0, -4.2, 4.2, -inf, +inf, nan
+
+// Learning points
+// - difference between float and double
+// - what is inff, inf, nan
+
+typedef enum e_type {
+    NONE,
+    INVALID,
+    CHAR,
+    INT,
+    FLOAT,
+    DOUBLE,
+    MY_NAN,
+    INF
+}   t_type;
+
+class ScalarConverter {
+    public:
+        // Constructors
+        ScalarConverter();
+        ScalarConverter(const std::string str);
+        ScalarConverter(const ScalarConverter &copy);
+
+        // Destructor
+        ~ScalarConverter(); // not using virtual because no other class is derived from this
+
+        // Operators
+        ScalarConverter &operator=(const ScalarConverter &other);
+
+        // Getters
+        std::string get_input() const;
+        t_type get_type() const;
+
+        // Setters
+
+        // Functions
+        void convert(const std::string &input); // should only take in string representation of char, int, float, double
+        // Note: If after convert to char, and is non printable then -> print an infor message
+        // Take in as string, convert to actual type, then return as explicit datatype
+
+    private:
+        std::string _input;
+        t_type _type;
+        void _main_set_type();
+        void _set_type_int_float_double();
+};
+
+#endif
