168816066812fd745f3a909a46fbdf7f603a21a3
Zheng Xiang <wong.zhengxiang@gmail.com>
2023-09-26 15:43:30 +0800

    REVIEW: Added comments ready for eval

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,18 +6,20 @@
 /*   By: zwong <zwong@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 18:35:20 by zwong             #+#    #+#             */
-/*   Updated: 2023/07/25 10:05:43 by zwong            ###   ########.fr       */
+/*   Updated: 2023/09/26 15:28:06 by zwong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Serializer.hpp"
 
 // What does it mean to serialize something?
-// Serialization is the process of converting a data structure to a format that
-// can be stored in a storage place or sent over a computer network and then reconfigured
-// in another computer environment.
+// -    Data serialization is the process of converting an object into a stream of bytes to more easily save or transmit it.
+// -    Serialization is the process of converting a data structure to a format that
+//      can be stored in a storage place or sent over a computer network and then reconfigured
+//      in another computer environment.
 // Serialization saves an item as a byte string in storage.
 // Learning about reinterpret_cast (more than static_cast, converts pointer to different types)
+// It works in it's own magical ways
 int main() {
     std::cout << MAGENTA << "----- Creating Data struct -----" << std::endl << RESET;
     Data data = Data();
