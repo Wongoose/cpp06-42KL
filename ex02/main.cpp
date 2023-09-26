d89b577dcb70ac85182f02dbb3baa12c62fcb1fa
Zheng Xiang <wong.zhengxiang@gmail.com>
2023-09-26 15:43:00 +0800

    REVIEW: Added comments to understand casts better

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,18 +6,24 @@
 /*   By: zwong <zwong@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 18:35:20 by zwong             #+#    #+#             */
-/*   Updated: 2023/07/27 00:36:04 by zwong            ###   ########.fr       */
+/*   Updated: 2023/09/26 15:42:37 by zwong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Base.hpp"
 
 // Learning about dynamic_cast<T>, where it will return 0 if the type is not the same
+// So in a way it does this:
+//  - Tries to cast to a type (trial and error)
+//  - Used on a base pointer (*) -> to identify the derived class (got it?)
+
+// Note: Derived class can be represented as a "base pointer (*)"
 // However, it is different when dynamic_cast a pointer vs reference (pointer is best practice)
 int main() {
     std::cout << MAGENTA << "----- Testing random generator of Base class -----" << std::endl << RESET;
     Base *base_array[10];
 	for (int i = 0; i < 10; i++) {
+        // Generate is random so I wouldn't know what derived type I got (represented/hidden as Base *)
 		base_array[i] = generate();
 		std::cout << "[" << i << "] class is (pointer): ";
 		identify(base_array[i]);
